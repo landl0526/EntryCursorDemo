e2fd0d501d9badb2479159357a0519ea7bd3f1e6
Land Lu <v-luting@microsoft.com>
2018-09-07 17:18:22 +0800

    Add project files.

---
+++ EntryCursorDemo/EntryCursorDemo.Android/EntryRendererForAndroid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EntryCursorDemo;
+using EntryCursorDemo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(EntryRendererForAndroid))]
+namespace EntryCursorDemo.Droid
+{
+    public class EntryRendererForAndroid : EntryRenderer
+    {
+        public EntryRendererForAndroid(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            IntPtr IntPtrtextViewClass = JNIEnv.FindClass(typeof(TextView));
+            IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(IntPtrtextViewClass, "mCursorDrawableRes", "I");
+            JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.my_cursor);
+        }
+    }
+}
---
+++ EntryCursorDemo/EntryCursorDemo.iOS/EntryRendererForiOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntryCursorDemo;
+using EntryCursorDemo.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(EntryRendererForiOS))]
+namespace EntryCursorDemo.iOS
+{
+    public class EntryRendererForiOS : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.TintColor = UIColor.White;
+        }
+    }
+}
